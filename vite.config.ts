e131422a389c37856469d7611d68f38593bc9bdf
StaticReasons <827391398@qq.com>
2024-10-04 15:48:35 +0800

    ci: :green_heart: fixed CI timeouts
    Added options to vite.config.ts
    This is a migration on vite for sass.
    Reference:
    https://sass-lang.com/documentation/breaking-changes/legacy-js-api/
    https://vite.dev/config/shared-options.html#css-preprocessoroptions

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  }
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern-compiler',
+			},
+		},
+	},
 })
