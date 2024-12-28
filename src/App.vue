b4858bc1874a5baebcc72861e994b60bf9c70533
OCCG <orangeawa@outlook.com>
2024-12-28 22:39:02 +0800

    feat: :sparkles: 添加备案信息及样式

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
     <title>{{ title }}</title>
     <navbar v-show="showBar"></navbar>
     <router-view :key="$route.fullPath"></router-view>
+    <!-- 备案信息 -->
+    <div class="footer">
+        © 2024 Redstone Daily | 
+        <a href="https://beian.miit.gov.cn/" target="_blank">闽ICP备2024058720号-2</a>
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -12,5 +17,15 @@ const showBar = ref(true);
 </script>
 
 <style scoped>
+.footer{
+    text-align: center;
+    color: white;
+    padding: 10px 10px;
+    background-color: var(--red-color);
+}
 
+.footer a{
+    color: white;
+    text-decoration: none;
+}
 </style>
